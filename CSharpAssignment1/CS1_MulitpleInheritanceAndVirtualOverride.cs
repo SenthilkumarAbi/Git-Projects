2ecd38aacf25e203d50acedebe4d2d878145e9eb
SenthilkumarAbi <senthil.mailto@gmail.com>
2018-11-19 10:53:35 +0530

    C# 2 assignments changes

---
+++ CSharpAssignment1/CS1_MulitpleInheritanceAndVirtualOverride.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class Program
+    {
+        delegate List<int> delegate_obj1(List<int> abc);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================*** 2D array with Size 3*3 ***============");
+            _TwoDArray p1 = new _TwoDArray();
+            p1.run();
+
+            Console.WriteLine("====================*** Multiple Inheritance ***============");
+            CS1_MulitpleInheritance p2 = new CS1_MulitpleInheritance();
+            p2.message();
+
+            Console.WriteLine("====================*** Abstract Virtual ***============");
+            childClass ch = new childClass();
+            ch.sum();
+
+            Console.WriteLine("====================*** Delegate Instances Example ***============");
+            
+            Math m = new Math();
+            delegate_obj1 _delegateobj = m.findAllDivisibleBy3;
+            List<int> numberlist = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 21, 30 };
+            Console.Write("Delegate Object calling divisible by 3 list from numlist   :  " );
+            _delegateobj(numberlist);
+
+            Console.WriteLine();
+            Console.WriteLine("====================*** Lambda Expression ***============");
+            Console.Write("Lambda Expression calling divisible by 3 list from numlist    ");
+            foreach (var item in numberlist.FindAll(j=> j%3==0))
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("====================*** Extend of System.String with function IsEmail() ***============");
+            Console.WriteLine("valid mailid Check for : senthil@test.com is {0}", commonUtilities.isEMail("senthil@test.com"));
+            Console.WriteLine("valid mailid Check for : senthiltest.com is {0}", commonUtilities.isEMail("senthiltest.com"));
+            Console.ReadLine();
+        }
+    }
+
+    #region  2DArray 3*3 Size
+    class _TwoDArray
+    {
+        //static void Main(string[] args)
+        public void run()
+        {
+
+            string[,] _matrix = new string[,]
+            {
+                {"AElement1","AElement2","AElement3" },
+                {"BElement1","BElement2","BElement3" },
+                {"CElement1", "CElement2","CElement3"}
+
+            };
+
+            for (int i = 0; i <= _matrix.GetUpperBound(0); i++)
+            {
+                Console.WriteLine("{0}  {1}  {2}", _matrix[i, 0], _matrix[i, 1], _matrix[i, 2]);
+            }
+ 
+
+        }
+    }
+    #endregion
+
+    #region MulitpleInheritance
+    class CS1_MulitpleInheritance : Ibase1   , Ibase2 
+    {
+        public  void message()
+        {
+            Console.WriteLine("Example Message from : Multiple Inheritance!..");
+        }
+    }
+
+    public interface Ibase1
+    {
+        void message();
+    }
+
+    public interface Ibase2
+    {
+        void message();
+    }
+    #endregion
+
+    #region AbstractVirtualOverride
+   
+    abstract class _abstractBaseClass
+    {
+        public int Number = 100;
+        public abstract void sum();
+    }
+
+    class childClass : _abstractBaseClass
+    {
+        public override void sum()
+        {
+            Console.WriteLine("Overrided Sum value :{0}", (Number + Number));    
+        }
+    }
+    #endregion
+
+    #region Delegate Instances
+    class Math
+    {
+        public List<int> findAllDivisibleBy3(List<int> numlist)
+        {
+            List<int> result = numlist.FindAll((int i) => { return i % 3 == 0; });
+            foreach (var item in result)
+            {
+                Console.Write(item + ",");
+            }
+           
+            return result;
+        }
+    }
+    #endregion
+
+    #region Extend String by IsEmail RegEx
+    public static class commonUtilities
+    {
+        public static bool isEMail(string input)
+        {
+            var result = Regex.Match(input, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            return result.Success;
+        }
+    }
+    #endregion
+}
---
+++ CSharpAssignment1/CS2_Assignment.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Student("ramesh",78.45);
+            Student_Result(p1.isOutStanding());
+            
+            Console.WriteLine("**************************");
+            Person p2 = new Student("suresh", 86.45);
+            Student_Result(p2.isOutStanding());
+            Console.WriteLine("**************************");
+            Person p3 = new Student("senthil", 94.5);
+            Student_Result(p3.isOutStanding());
+            Console.WriteLine("**************************");
+            Person p4 = new Professor("Bala", 7);
+            Professor_Result(p4.isOutStanding());
+            Console.WriteLine("**************************");
+            Person p5 = new Professor("Murugan", 4);
+            Professor_Result(p5.isOutStanding());
+            Console.WriteLine("**************************");
+            Console.ReadLine();
+        }
+
+        static void Student_Result(bool result)
+        {
+            if (result)
+                Console.WriteLine("This Student is secured more than 85 and he is outstanding!..");
+            else
+                Console.WriteLine("This Student is did not secured more than 85 and he is not outstanding!..");
+        }
+
+        static void Professor_Result(bool result)
+        {
+            if (result)
+                Console.WriteLine("This Professor is published  more than 4 and he is outstanding!..");
+            else
+                Console.WriteLine("This Professor is did not published more than 4 and he is not outstanding!..");
+        }
+
+    }
+
+    public  class Person
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+        public Person() { }
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        virtual public bool isOutStanding() { return true; }
+
+        
+       
+    }
+    public class Professor : Person
+    {
+
+        public Professor(string name, int booksPublished)
+        {
+            this.name = name;
+            this.booksPublished = booksPublished;
+          
+        }
+        public int booksPublished { get; set; }
+
+        public override bool isOutStanding()
+        {
+            Console.WriteLine( Display());
+            return booksPublished > 4;
+        }
+
+        public string Display()
+        {
+            return "Professor " + this.name + " has Published " + Convert.ToString(booksPublished) + " number of books !..";
+        }
+    }
+    public class Student : Person
+    {
+        public Student(string name, double percentage)
+        {
+            this.name = name;
+            this.percentage = percentage;
+           
+        }
+        public double percentage { get; set; }
+
+        public override bool isOutStanding()
+        {
+            Console.WriteLine(print());
+            return percentage > 85;
+        }
+
+        public string print()
+        {
+            return "Student " + this.name + " secured percentage :" + Convert.ToString(percentage) + " !..";
+        }
+    }
+}
